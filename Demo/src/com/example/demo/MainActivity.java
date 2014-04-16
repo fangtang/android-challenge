016e446b57af282675ce0e095cd60cccbe9a7990
ny <niyang132@163.com>
2014-04-16 20:28:14 +0800

    ver 1.0

---
+++ Demo/src/com/example/demo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MainActivity extends Activity {
+	private SimpleAdapter simpleAdapter;
+	private ListView curListView;
+	private MyTask myTask;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		myTask = new MyTask();
+		myTask.execute(MainActivity.this);
+	}
+ 
+	private class MyTask extends AsyncTask<Context, Integer, List<HashMap<String, String>>>{
+		private Context context;
+
+		@Override
+		protected List<HashMap<String, String>> doInBackground(Context...params) {
+			// TODO 自动生成的方法存根
+			this.context = params[0];
+			GetRepoAttri gr = new GetRepoAttri();
+			return gr.getMsgList("rails", "rails");
+		}
+		@Override
+		protected void onPostExecute(List<HashMap<String, String>> list){
+			curListView = (ListView) findViewById(R.id.msglist);
+			simpleAdapter = new SimpleAdapter(context,list,R.layout.item, 
+					new String[]{"name","date","content"}, new int[]{R.id.name,R.id.date,R.id.content});
+			curListView.setAdapter(simpleAdapter);
+		}
+	}
+}
